a46069e81fd090162c474ef08796f3e4089eb76a
Raulugs <raul.sanchezg@udem.edu.mx>
2019-10-22 13:55:00 -0500

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace TeleprompterConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ShowTeleprompter().Wait();
+        }
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                var words = line.Split(' ');
+                var lineLength = 0;
+                foreach (var word in words)
+                {
+                    yield return word + " ";
+                    lineLength += word.Length +1;
+                    if (lineLength > 70)
+                    {
+                        yield return Environment.NewLine;
+                        lineLength = 0;
+                    }
+
+                }
+                yield return Environment.NewLine;
+
+                }
+            }
+        }
+        private static async Task ShowTeleprompter()
+        {
+            var words = ReadFrom("C:\\sampleQuotes.txt");
+            foreach (var word in words)
+            {
+                Console.Write(word);
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    await Task.Delay(200);
+                }
+            }
+        }
+
+                private static async Task GetInput()
+        {
+            var delay = 200;
+            Action work = () =>
+            {
+                do {
+                    var key = Console.ReadKey(true);
+                    if (key.KeyChar == '>')
+                    {
+                        delay -= 10;
+                    }
+                    else if (key.KeyChar == '<')
+                    {
+                        delay += 10;
+                    }
+                    else if (key.KeyChar == 'X' || key.KeyChar == 'x')
+                    {
+                        break;
+                    }
+                } while (true);
+            };
+            await Task.Run(work);
+        }
         }
     }
+    
 }
